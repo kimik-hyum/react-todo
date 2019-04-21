5aaaba91da257890a3aab6f8480330fd02594c4c
kimik <familykim01@naver.com>
2019-04-21 20:40:10 +0900

    JSX markup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './css/App.scss';
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hi, my name is {this.props.name}</div>;
+    return (
+      <div className="container">
+        <div className="head-area">
+          <h1>React To do list</h1>
+        </div>
+        <div className="input-area">
+          <input type="text" text=""/>
+          <button>추가</button>
+        </div>
+        <div className="list-area">
+          <ul>
+            <li><span>할일 목록1</span> <button className="del">삭제</button><button className="edit">편집</button></li>
+            <li><span>할일 목록2</span> <button className="del">삭제</button><button className="edit">편집</button></li>
+            <li><span>할일 목록3</span> <button className="del">삭제</button><button className="edit">편집</button></li>
+            <li><span>할일 목록4</span> <button className="del">삭제</button><button className="edit">편집</button></li>
+          </ul>
+        </div>
+      </div>
+    )
   }
 }
 
