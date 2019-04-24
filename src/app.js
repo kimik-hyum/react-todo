1b1b9c92b4c971efe90b8e538992a2de9726a956
kimik <familykim01@naver.com>
2019-04-24 22:41:16 +0900

    JSX + style 작업

--- src/app.js
+++ src/app.js
@@ -1,30 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './css/App.scss';
+import TodoInput from './component/input';
+import TodoList from './component/todo';
+import './scss/App.scss';
 
-class HelloMessage extends React.Component {
+class ReactTodo extends React.Component {
   render() {
     return (
       <div className="container">
         <div className="head-area">
           <h1>React To do list</h1>
         </div>
-        <div className="input-area">
-          <input type="text" text=""/>
-          <button>추가</button>
-        </div>
-        <div className="list-area">
-          <ul>
-            <li><span>할일 목록1</span> <button className="del">삭제</button><button className="edit">편집</button></li>
-            <li><span>할일 목록2</span> <button className="del">삭제</button><button className="edit">편집</button></li>
-            <li><span>할일 목록3</span> <button className="del">삭제</button><button className="edit">편집</button></li>
-            <li><span>할일 목록4</span> <button className="del">삭제</button><button className="edit">편집</button></li>
-          </ul>
-        </div>
+        <TodoInput />
+        <TodoList />
       </div>
     )
   }
 }
 
 var app = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Kimik" />, app);
+ReactDOM.render(<ReactTodo name="Kimik" />, app);
