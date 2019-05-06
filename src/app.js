32afd29b6e920f8586f3f3b963dceb2f3cb42ae7
kimik <familykim01@naver.com>
2019-05-06 16:59:23 +0900

    reset

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TodoInput from './component/input';
+import TodoList from './component/todo';
+import './scss/App.scss';
 
-class HelloMessage extends React.Component {
+class ReactTodo extends React.Component {
   render() {
-    return <div>Hi, my name is {this.props.name}</div>;
+    return (
+      <div className="container">
+        <div className="head-area">
+          <h1>React To do list</h1>
+        </div>
+        <TodoInput />
+        <TodoList />
+      </div>
+    )
   }
 }
 
 var app = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Kimik" />, app);
+ReactDOM.render(<ReactTodo name="Kimik" />, app);
